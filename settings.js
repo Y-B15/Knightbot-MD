c227666db7fb53d090a9895b8d40fb829025659d
Y-B15 <114311644+Y-B15@users.noreply.github.com>
2025-02-09 20:33:10 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'FXLLS',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "FXLLS Bot",
+  botOwner: 'FXLLS', // Your name
+  ownerNumber: '+212684390588', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
